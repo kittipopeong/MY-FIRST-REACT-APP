d8e16a0d925b6a8a68862cbfd65a1f9c343bf3d8
Johnyong <johnyong@MacBook-Pro-khxng-Johnyong.local>
2023-09-29 14:47:36 +0700

    xxx

---
+++ src/component/Greeting.tsx
@@ -0,0 +1,12 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>welcome</h3>
+      <p>Kittipope</p>
+    </div>
+  )
+}
+
+export default Greeting
