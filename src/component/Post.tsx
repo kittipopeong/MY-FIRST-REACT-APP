e98a2a3ceacdf66330821e7635b0a9eb928714ba
Johnyong <johnyong@MacBook-Pro-khxng-Johnyong.local>
2023-10-03 14:48:52 +0700

    form

--- src/component/Post.tsx
+++ src/component/Post.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { PostDTO } from '../types/dto'
 import classes from './Post.module.css'
 
@@ -6,15 +7,20 @@ interface IPostProps {
 }
 
 const Post = ({ post }: IPostProps) => {
-  const handleClick = (id: number) => {
-    alert(`clicked ${id}`)
+  const [buttonMsg, setButtonMsg] = useState<boolean>(false)
+
+  const toggleShow = () => {
+    setButtonMsg(!buttonMsg)
   }
+
   return (
-    <div onClick={() => handleClick(post.id)} className={classes.post}>
+    <div className={classes.post}>
       <p>id: {post.id}</p>
       <p>postedBy: {post.userId}</p>
       <p>title: {post.title}</p>
       <p>body: {post.body}</p>
+      {buttonMsg && <p> More post info</p>}
+      <button onClick={toggleShow}>{buttonMsg ? 'Show Less' : ' Show more'}</button>
     </div>
   )
 }
